8d12b36d1fabad37049f82b73ba1b14311f9f86d
Yohannes Garomsa <yohannesgaromsa051@gmail.com>
2026-01-27 09:06:40 +0300

    Added Bootstrap styling and total assets calculation

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .models import Account
+from django.db.models import Sum
+
 
 def account_list(request):
     # Fetch all bank accounts from the MySQL database
     all_accounts = Account.objects.all() 
-    return render(request, 'index.html', {'accounts': all_accounts})
+    total =all_accounts.aggregate(Sum('balance'))['balance__sum'] or 0
+    return render(request, 'index.html', {
+        'accounts': all_accounts,
+        'total_assets': total})
