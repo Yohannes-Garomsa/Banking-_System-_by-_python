12db789cc6586d9cda305a2b72aa83e2bcb9342e
Yohannes Garomsa <yohannesgaromsa051@gmail.com>
2026-01-27 09:26:33 +0300

    Added real-time search and dynamic assets calculation

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from .models import Account
-from django.db.models import Sum
+from django.db.models import Sum ,Q
 
 
 def account_list(request):
-    # Fetch all bank accounts from the MySQL database
-    all_accounts = Account.objects.all() 
-    total =all_accounts.aggregate(Sum('balance'))['balance__sum'] or 0
-    return render(request, 'index.html', {
+   
+      query =request.GET.get('search')
+      if query:
+          #filter accounts where the owner's name contains the search query (case-insensitive)
+          all_accounts =Account.objects.filter(owner__icontains=query)
+      else:
+          all_accounts=Account.objects.all()
+      # 2. Recalculate total assets based on what is filtered
+      total =all_accounts.aggregate(Sum('balance'))['balance__sum'] or 0
+      return render(request, 'index.html', {
         'accounts': all_accounts,
-        'total_assets': total})
+        'total_assets': total,
+        'query': query # Send the query back so it stays in the search bar
+        })  
